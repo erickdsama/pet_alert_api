8344657be0cfa856052b2fa49f41a3b6fb109186
erick <erickdsama@gmail.com>
2020-03-31 22:08:30 -0600

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template of FLASK & PSQL with docker
+
+**requirements**
+
+- docker: https://docs.docker.com/install/
+
+- docker-compose: https://docs.docker.com/compose/install/
+
+First your need clone your project.
+
+insisde of project directory run:
+
+`docker-compose up`
--- boot.sh
+++ boot.sh
@@ -2,9 +2,4 @@
 flask db init
 flask db migrate
 flask db upgrade
-if [ $FLASK_ENV = "development" ]
-then
-  exec gunicorn -b :5001 --access-logfile - --error-logfile - main:app  --reload
-else
-  exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app  --reload
-fi
+exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app  --reload
