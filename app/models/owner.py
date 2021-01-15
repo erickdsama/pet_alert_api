9deb4322e0b50effb2045e0ee7709c8d1dd000f6
Erick <erickdsama@gmail.com>
2021-01-14 22:09:23 -0700

    email unique

--- app/models/owner.py
+++ app/models/owner.py
@@ -10,7 +10,7 @@ class Owner(db.Model):
     name = Column(String(30))
     last_name = Column(String(30))
     username = Column(String(100))
-    email = Column(String(200))
+    email = Column(String(200), unique=True)
     phone = Column(String(14))
     f_user = Column(String(150))
     g_user = Column(String(150))
